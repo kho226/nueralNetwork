c64e33dbd7c159daed2c3abc8f164072e8204514
Kyle Ong <kho226@nyu.edu>
2016-12-25 12:23:51 -0800

    basic skeleton of nueral network class

---
+++ nueralNetwork.py
@@ -0,0 +1,20 @@
+class nueralNetwork:
+    #nueral network initializer
+    def __init__(self, numInputNodes, numHiddenNodes, numOutputNodes, learningFactor):
+        self.inputNodes = numInputNodes
+        self.hiddenNodes = numHiddenNodes
+        self.outputNodes = numOutputNodes
+
+        #learning factor - ⍺
+        self.lf = learningFactor
+        pass
+
+    #query the data
+    def query(self):
+        pass
+
+    #train the nueral network
+    def train(self):
+        pass
+    
+        
