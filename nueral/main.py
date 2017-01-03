297246e7d36b48c1e9733c3dfc9206ae3729f366
Kyle Ong <kho226@nyu.edu>
2017-01-03 14:33:47 -0800

    updated interface to fetch training data from csv file

--- nueral/main.py
+++ nueral/main.py
@@ -1,6 +1,7 @@
 #! usr/bin/python
 from __future__ import print_function
 from nueralNetwork import nueralNetwork
+from pathlib import Path
 
 def main ():
         print ("Enter the dimensions for your network...")
@@ -10,7 +11,22 @@ def main ():
         print ("Enter a learning - factor for your network...")
         learningFactor = input("learning factor: ")
         nueralNet = nueralNetwork(numInputNodes,numHiddenNodes,numInputNodes, learningFactor)
+        #grab the training dats
+        userInput = input("Enter the file path of your training data:")
+        filePath = Path(userInput) 
+        print (filePath)
+        dataList = []
+        if filePath.is_file():
+            dataFile = open(userInput, 'r')
+            dataList = dataFile.readlines()
+            dataFile.close()
+        else:
+            print (str(filePath) + " does not exist")
+        print ("printing out the first value in dataList, the pixel color values associated with the first value, and the length of dataList.")
+        print (dataList[0])
+        print (len(dataList))
         
+         
     
 if __name__ == '__main__':
         main()
